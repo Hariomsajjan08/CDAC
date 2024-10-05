736e45c2157af895f7e5ea2463386c8c7a112bd9
Hariomsajjan08 <Hariomsajjan08@gmail.com>
2024-10-06 00:15:45 +0530

    assi4

--- Tree.java
+++ Tree.java
@@ -1,68 +1,74 @@
-class Tree{
-	//starting point of the tree
-	Node root;
-	
-	//Node structure
-	static class Node{
-		int data;
-		Node left;
-		Node right;
-		
-		Node(int value){
-			this.data=value;
-			left=right=null;
-		}
-	}
-	
-	//kepting root as null
-	Tree(){
-		root=null;
-	}
-	
-	//initiazlie first node 
-	Tree(int value){
-		root = new Node(value);
-	}
-	
-	Node  insert(Node root, int value){
-		//tree is empty then create new node
-		if(root == null){
-			root = new Node(value);
-			return root;
-		}
-		if(value<=root.data){
-			root.left = root.insert(root.left,value);
-		}else{
-			root.right=root.insert(root.right,value);
-		}
-	}
-	
-	Node deletion(Node root, int value){
-		if(root==null){
-			return root;
-		}
-		if(value<.root.data){
-			root.left = root.deletion(root.left,value);
-		}else if(value>root.data){
-			root.right=root.deletion(root.right,value);
-		}else{
-			if(root.left==null){
-				return root.right;
-			}else if(root.right==null){
-				return root.left;
-			}
-			//case3
-			root.data=minValue(root.right);
-			rood.right=deletion(root.right,root.data);
-			
-		}
-		in minValue(Node root){
-			int x = root.data;
-			while(root.left!=null){
-				x=root.left.data;
-				root=root.left;
-			}
-			return x;
-		}
-	}
+class Tree {
+    // Starting point of the tree
+    Node root;
+
+    // Node structure
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.data = value;
+            left = right = null;
+        }
+    }
+
+    // Keeping root as null
+    Tree() {
+        root = null;
+    }
+
+    // Initialize first node
+    Tree(int value) {
+        root = new Node(value);
+    }
+
+    // Insert method
+    Node insert(Node root, int value) {
+        // Tree is empty, create new node
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+        if (value <= root.data) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    // Deletion method
+    Node deletion(Node root, int value) {
+        if (root == null) {
+            return root;
+        }
+        if (value < root.data) {
+            root.left = deletion(root.left, value);
+        } else if (value > root.data) {
+            root.right = deletion(root.right, value);
+        } else {
+            // Node with one or no child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // Node with two children
+            root.data = minValue(root.right);
+            root.right = deletion(root.right, root.data);
+        }
+        return root;
+    }
+
+    // Helper method to find the minimum value in a tree
+    int minValue(Node root) {
+        int minVal = root.data;
+        while (root.left != null) {
+            minVal = root.left.data;
+            root = root.left;
+        }
+        return minVal;
+    }
 }
